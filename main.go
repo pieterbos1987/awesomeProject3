c40a69cf5d4d558c81542de8509bff4fa5116428
Pieter Bos <pieterbos1987@protonmail.com>
2022-01-10 08:02:13 +0100

    reverting back

--- main.go
+++ main.go
@@ -17,22 +17,9 @@ func contact(w http.ResponseWriter, r *http.Request) {
 		"<a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>")
 }
 
-func faq(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>This is the FAQ page!</h1>")
-}
-
-func notFound(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	w.WriteHeader(http.StatusNotFound)
-	fmt.Fprint(w, "<h1>404 Page Not Found</h1>")
-}
-
 func main() {
 	r := mux.NewRouter()
-	r.NotFoundHandler = handle404
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
-	r.HandleFunc("/faq", faq)
 	http.ListenAndServe(":3000", r)
 }
